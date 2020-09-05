c6e2554285f066764af3b2f5b9317bb70d57eae3
Finnbar Keating <finnjkeating@aol.com>
2020-09-05 14:40:49 +0100

    Formatting fixes Look I like my style okay

--- ALTANTIS/world/world.py
+++ ALTANTIS/world/world.py
@@ -1,15 +1,13 @@
 """
 Deals with the world map, which submarines explore.
 """
-import operator
 import string
 from functools import reduce
 
 from ALTANTIS.utils.text import list_to_and_separated
 from ALTANTIS.utils.direction import reverse_dir, directions
 from ALTANTIS.utils.consts import X_LIMIT, Y_LIMIT
-from ALTANTIS.world.validators import InValidator, NopValidator, TypeValidator, BothValidator, LenValidator, \
-    RangeValidator
+from ALTANTIS.world.validators import InValidator, NopValidator, TypeValidator, BothValidator, LenValidator, RangeValidator
 from ALTANTIS.world.consts import ATTRIBUTES, WEATHER, WALL_STYLES
 
 import random
@@ -229,23 +227,18 @@ class Cell():
             return True
         return False
 
-
 undersea_map = [[Cell() for _ in range(Y_LIMIT)] for _ in range(X_LIMIT)]
-
 
 def in_world(x: int, y: int) -> bool:
     return 0 <= x < X_LIMIT and 0 <= y < Y_LIMIT
 
-
 def possible_directions() -> List[str]:
     return list(directions.keys())
-
 
 def get_square(x: int, y: int) -> Optional[Cell]:
     if in_world(x, y):
         return undersea_map[x][y]
     return None
-
 
 def bury_treasure_at(name: str, pos: Tuple[int, int]) -> bool:
     (x, y) = pos
@@ -253,19 +246,16 @@ def bury_treasure_at(name: str, pos: Tuple[int, int]) -> bool:
         return undersea_map[x][y].bury_treasure(name)
     return False
 
-
 def pick_up_treasure(pos: Tuple[int, int], power: int) -> List[str]:
     (x, y) = pos
     if in_world(x, y):
         return undersea_map[x][y].pick_up(power)
     return []
 
-
 def map_tick():
     for x in range(X_LIMIT):
         for y in range(Y_LIMIT):
             undersea_map[x][y].cell_tick()
-
 
 def map_to_dict() -> Dict[str, Any]:
     """
@@ -278,7 +268,6 @@ def map_to_dict() -> Dict[str, Any]:
         for j in range(Y_LIMIT):
             undersea_map_dicts[i][j] = undersea_map[i][j]._to_dict()
     return {"map": undersea_map_dicts, "x_limit": X_LIMIT, "y_limit": Y_LIMIT}
-
 
 def map_from_dict(dictionary: Dict[str, Any]):
     """
