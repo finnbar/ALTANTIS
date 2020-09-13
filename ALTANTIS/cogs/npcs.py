19c7dbb5b6f555b725f3336d6ae63b157bbd86cd
Finnbar Keating <finnjkeating@aol.com>
2020-09-13 10:47:20 +0100

    Added mutation of enemies

--- ALTANTIS/cogs/npcs.py
+++ ALTANTIS/cogs/npcs.py
@@ -6,7 +6,7 @@ from ALTANTIS.utils.bot import perform_unsafe, get_team, perform_async_unsafe
 from ALTANTIS.utils.actions import DiscordAction, Message, OKAY_REACT, FAIL_REACT
 from ALTANTIS.utils.text import list_to_and_separated
 from ALTANTIS.subs.state import get_sub
-from ALTANTIS.npcs.npc import add_npc, kill_npc, get_npc_types
+from ALTANTIS.npcs.npc import add_npc, get_npc, kill_npc, get_npc_types
 
 class NPCs(commands.Cog):
     """
@@ -35,6 +35,14 @@ class NPCs(commands.Cog):
         (CONTROL) Prints out all possible NPC types.
         """
         await perform_unsafe(printout_npc_types, ctx)
+    
+    @commands.command()
+    @commands.has_role(CONTROL_ROLE)
+    async def mutate(self, ctx, npc : int, health : int):
+        """
+        (CONTROL) Sets an NPC <npc>'s health to <health>.
+        """
+        await perform_unsafe(set_npc_health, ctx, npc, health)
 
 def add_npc_to_map(ntype : str, x : int, y : int, team : Optional[str]) -> DiscordAction:
     sub = None
@@ -50,3 +58,12 @@ async def remove_npc_from_map(npcid : int, rattle : bool) -> DiscordAction:
 def printout_npc_types() -> DiscordAction:
     types = list_to_and_separated(get_npc_types())
     return Message(f"Possible NPC types: {types}.")
+
+def set_npc_health(npcid : int, health : int) -> DiscordAction:
+    if health <= 0:
+        return FAIL_REACT
+    npc = get_npc(npcid)
+    if npc is not None:
+        npc.health = health
+        return OKAY_REACT
+    return FAIL_REACT
